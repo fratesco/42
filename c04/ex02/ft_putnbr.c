e9e99774b1dfb9243de8c26d90b53a515861c067
Francesco Golino <fgolino@e3r4p6.42roma.it>
2022-10-24 01:05:53 +0200

    salvataggio

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgolino <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 10:24:31 by fgolino           #+#    #+#             */
+/*   Updated: 2022/10/16 18:19:56 by fgolino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+int	potenza(int e, int b)
+{
+	int	base;
+
+	base = b;
+	while (e - 1 > 1)
+	{
+		b = b * base;
+		e--;
+	}
+	return (b);
+}
+
+int	cifre(int nb)
+{
+	int	c;
+
+	c = 0;
+	while (nb != 0)
+	{
+		nb = nb / 10;
+		c++;
+	}
+	return (c);
+}
+
+void	positivo(int nb)
+{
+	int		a;
+	char	scritta;
+	char	ultima_cifra;
+
+	a = cifre(nb);
+	while (a > 1)
+	{
+		scritta = nb / potenza(a, 10) + '0';
+		write(1, &scritta, 1);
+		ultima_cifra = nb / potenza(a, 10);
+		nb = nb - (ultima_cifra * potenza(a, 10));
+		a--;
+	}
+	scritta = nb + '0';
+	write(1, &scritta, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{		
+		nb = nb * (-1);
+		write(1, "-", 1);
+	}
+	positivo(nb);
+}
