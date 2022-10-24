b958bfe9f73476e5c913f5eab9543b34c88df6f3
Francesco Golino <fgolino@e3r4p6.42roma.it>
2022-10-24 05:52:06 +0200

    salvataggio

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgolino <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 05:06:07 by fgolino           #+#    #+#             */
+/*   Updated: 2022/10/24 05:11:36 by fgolino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0 && nb == 0)
+		return (1);
+	if (power == 0)
+		return (1);
+	return (nb *= (ft_recursive_power(nb, power - 1)));
+}
---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgolino <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 05:14:32 by fgolino           #+#    #+#             */
+/*   Updated: 2022/10/24 05:35:06 by fgolino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	return (index = ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
