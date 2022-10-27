09337a8cd24adc798c05ae10095fa16c665ad28e
Francesco Golino <fgolino@e3r1p3.42roma.it>
2022-10-27 18:40:33 +0200

    salvataggio

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: fgolino <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 08:23:17 by fgolino           #+#    #+#             */
-/*   Updated: 2022/10/27 18:34:53 by fgolino          ###   ########.fr       */
+/*   Updated: 2022/10/27 18:40:27 by fgolino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 int	ft_utltimate_range(int **range, int min, int max)
 {
 	int	size;
-	int	*vec;
 	int	i;
 
 	if (min >= max)
 		return (0);
 	size = (unsigned int)(max - min);
 	range = (int **)malloc(1 * sizeof(int *));
-	vec = (int *)malloc(size * sizeof(int));
-	if (!(range || vec))
+	range[0] = (int *)malloc(size * sizeof(int));
+	if (!(range))
 		return (-1);
 	i = 0;
 	while (i < size && min < max)
