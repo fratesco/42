41d6967412394359cfddb6e16a15e90ea91b89f7
Francesco Golino <fgolino@e3r1p7.42roma.it>
2022-11-01 11:56:44 +0100

    salvataggio

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,70 +6,73 @@
 /*   By: fgolino <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:01:19 by fgolino           #+#    #+#             */
-/*   Updated: 2022/10/31 17:02:07 by fgolino          ###   ########.fr       */
+/*   Updated: 2022/11/01 11:56:16 by fgolino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-int str_len(char *str)
-{
-    int i;
 
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+int	str_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char    *str_cat(char *str1, char *str2, int len)
+int	tot_length(char **strs, int size)
 {
-        int i;
+	int	i;
+	int	tot_len;
 
-        i = 0;
-        while (str2[i])
-        {
-            str1[len + 1] = str2[i];
-            i++;
-        }
-        str1[len + 1] = 0;
-        return (str1);
+	i = 0;
+	tot_len = 0;
+	while (i < size)
+	{
+		tot_len += str_len(strs[i]);
+		i++;
+	}
+	return (tot_len);
 }
 
-char    *ft_strjoin(int size, char **strs, char *sep)
+char	*str_cat(char *str1, char *str2, int len)
 {
-        int     i;
-        char    *str;
-        int     tot_len;
+	int	i;
 
-        if (size == 0)
-        {
-            str = (char *)malloc(sizeof(char) * 1);
-            return (str);
-        }
-        tot_len = 0;
-        i = 0;
-        while (1 < size)
-        {
-            tot_len += str_len(strs[i]) - 1;
-            i++;
-        }
-        tot_len += (str_len(sep) * (size - 1)) - (size - 1);
-        str = (char *)malloc(sizeof(char) * tot_len);
-        i = 0;
-        str[tot_len - 1] = 0;
-        while (i < size)
-        {
-            str_cat(str, strs[i], str_len(str));
-            i++;
-            str_cat(str, sep, str_len(str));   
-        }
-        return (str);
+	i = 0;
+	while (str2[i])
+	{
+		str1[len + i] = str2[i];
+		i++;
+	}
+	str1[len + i] = 0;
+	return (str1);
 }
 
-int main (int argc, char **argv)
+char	*ft_strjoin(int size, char **strs, char *sep)
 {
-    printf("%s",ft_strjoin((argc - 1), argv, "nanni"));
-    return (0);
+	int		i;
+	char	*str;
+	int		tot_len;
 
+	if (size == 0)
+	{
+		str = (char *)malloc(sizeof(char) * 1);
+		return (str);
+	}
+	tot_len = tot_length(strs, size);
+	tot_len += (str_len(sep) * (size - 1)) + 1;
+	str = (char *)malloc(sizeof(char) * tot_len);
+	while (tot_len > 0)
+		str[--tot_len] = 0;
+	i = 0;
+	while (i < size)
+	{
+		str_cat(str, strs[i++], str_len(str));
+		if (i < size)
+			str_cat(str, sep, str_len(str));
+	}
+	return (str);
 }
