4e47b53d3fa5805ac9353bce89e5133b97116b0e
Francesco Golino <fgolino@e3r4p4.42roma.it>
2023-01-23 14:49:58 +0100

    salvataggio

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: fgolino <fgolino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:38:34 by fgolino           #+#    #+#             */
-/*   Updated: 2023/01/21 14:17:44 by fgolino          ###   ########.fr       */
+/*   Updated: 2023/01/23 14:42:00 by fgolino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,5 @@ char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
 char			*ft_itoa(int n);
+char			*ft_strmapi(char const *s, char (*f)(unsigend int, char));
 #endif
