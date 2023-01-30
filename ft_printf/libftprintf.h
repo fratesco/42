d6fde45ea51bbe81b0af636e1f5694fc3ee1b6bb
Francesco Golino <fgolino@e3r5p5.42roma.it>
2023-01-30 12:23:54 +0100

    salvataggio

---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgolino <fgolino@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 12:07:36 by fgolino           #+#    #+#             */
+/*   Updated: 2023/01/30 12:12:34 by fgolino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+int		ft_printf(const char *, ...);
+
+#endif
