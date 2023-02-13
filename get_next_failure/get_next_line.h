0ee57ea0614bc1ad4e43d9171382028d29938033
Francesco Golino <fgolino@e4r4p4.42roma.it>
2023-02-13 12:38:29 +0100

    salvataggio

---
+++ get_next_failure/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgolino <fgolino@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 17:44:33 by fgolino           #+#    #+#             */
+/*   Updated: 2023/02/10 16:56:38 by fgolino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# define FOUND 1
+# define NOTHING 0
+# define INVALID -1
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*flow_lines(char *str, int len, char **rest);
+int		check_new(char	*str);
+char	*hope_it_works(char *buffer, int len, int fd, char **rest);
+char	*get_next_line(int fd);
+int		ft_strlen(char const *str);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
