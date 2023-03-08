7c08385d8bffb305882c76a3ddb3f0a97c35e71f
fratesco <fratesco0@gmail.com>
2023-03-08 18:32:55 +0100

    salvataggio

--- minitalk/ft_printf/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "ft_printf.h"
-
-int main()
-{
-	ft_printf("%i", ft_printf("akjdhfkajkjshf% asfdjsaodfh%p %% %suca", 16, "s"));
-	printf("%i", printf("akjdhfkajkjshf% asfdjsaodfh%p %% %suca", 16, "s"));
-}
