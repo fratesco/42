8dfb5124522d71324288efb6cc1eddeaeb2422c7
Francesco Golino <fgolino@e4r9p1.42roma.it>
2023-03-09 13:09:18 +0100

    salvataggio

--- push_swap/instructions.c
+++ push_swap/instructions.c
@@ -6,7 +6,7 @@
 /*   By: fgolino <fgolino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 11:25:55 by fgolino           #+#    #+#             */
-/*   Updated: 2023/03/09 12:24:14 by fgolino          ###   ########.fr       */
+/*   Updated: 2023/03/09 13:08:39 by fgolino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,19 @@ void	swap(long **stack)
 	stack[0][1] = num;
 }
 
-void	push(long **stack_a, long **stack_b, int len_a, int len_b)
+void	push(long **stack_a, long **stack_b, int *len_a, int *len_b)
 {
 	int	i;
 
-	if (len_b == 0)
+	i = 0;
+	if (*len_b == 0)
 		return ;
-	while ()
+	(*stack_a)[(*len_b) - 1] = (*stack_b)[0];
+	while (i < len_b - 1)
 	{
-		
+		(*stack_b)[i] = (*stack_b)[i + 1];
+		i++;
 	}
+	*len_b -= 1;
+	*len_a += 1;
 }
