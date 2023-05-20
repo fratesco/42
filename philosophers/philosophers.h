bf912724d8bd8807f9fcdee49dd211d1c862db2d
fratesco <fratesco0@gmail.com>
2023-05-20 10:19:21 +0200

    salvataggio

---
+++ philosophers/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgolino <fgolino@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 13:32:37 by fgolino           #+#    #+#             */
+/*   Updated: 2023/05/18 13:37:36 by fgolino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <
