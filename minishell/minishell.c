825a915d1c748e9cc4d30faf5b169f4c0a94c221
fratesco <fratesco0@gmail.com>
2023-08-19 17:34:34 +0200

    boh

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: fgolino <fgolino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 18:47:49 by fgolino           #+#    #+#             */
-/*   Updated: 2023/08/19 17:30:22 by fgolino          ###   ########.fr       */
+/*   Updated: 2023/08/19 17:34:03 by fgolino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int	main(void)
 		// probabilmente è meglio se tutto quello che succede da qui in poi è gestito da un processo figlio
 		start();
 		//qui il processo genitori si mette in waitpid(0, NULL, NULL) e poi fa ripartire il suo loop
+		g_info.ins_pid = 0;
 		free(g_info.instruction);
 	}
 	free_matrix(g_info.global_path);
