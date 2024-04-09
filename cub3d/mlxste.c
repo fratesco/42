9464bb3a9a19c28d60035c68f42e080fc493151d
srapuano <srapuano@LINUX.myguest.virtualbox.org>
2024-04-09 14:54:13 +0200

    keys

--- cub3d/mlxste.c
+++ cub3d/mlxste.c
@@ -6,7 +6,7 @@
 /*   By: srapuano <srapuano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:04:23 by srapuano          #+#    #+#             */
-/*   Updated: 2024/04/08 16:33:19 by srapuano         ###   ########.fr       */
+/*   Updated: 2024/04/09 14:32:32 by srapuano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ void    mlx_handler(t_data *data)
     data->mlx = mlx_init();
 	data->window = mlx_new_window(data->mlx, 1280,
 			720, "CUB3D");
-	//put_img(data);
-	//mlx_key_hook(data->window,, data);
-	//mlx_hook(data->window, 17, 0, , (void *)data);
+	mlx_key_hook(data->window, key_controls, data);
+	mlx_hook(data->window, 17, 0, close_game, (void *)data);
 	mlx_loop_hook(data->mlx, nullifiereee, data);
 	mlx_loop(data->mlx);
 }
