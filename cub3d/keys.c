57946d5b7616fb767b08e9c5d95db588e4d6758d
fratesco <fratesco@ubuntu.myguest.virtualbox.org>
2024-04-27 16:31:12 +0200

    scommentato

--- cub3d/keys.c
+++ cub3d/keys.c
@@ -14,26 +14,23 @@
 
 int	keys_control(int kc, t_data *data)
 {
-	printf("%d\n", kc);
-	//if (kc == 65307 || kc == 100 || kc == 97 || kc == 115 || kc == 119)
-	//{
-		//if (kc == 65307)
-			//close_game(&data);
-		//keys_movements(kc, &data);
-		//mlx_clear_window(data->mlx, data->window);
-		//print_map(&data);
-	//}
+	if (kc == 65307 || kc == 100 || kc == 97 || kc == 115 || kc == 119)
+	{
+		if (kc == 65307)
+			close_game(&data);
+		keys_movements(kc, &data);
+	}
 	return (0);
 }
 
 void	keys_control_movements(int kc, t_data *data)
 {
-    //if (kc == 100) /*&& data->dead == FALSE */
-		//move_right(&data);
-	//if (kc == 97)/*&& data->dead == FALSE*/
-		//move_left(&data);
-	//if (kc == 115) /*&& data->dead == FALSE*/
-		//move_backwards(&data);
-	//if (kc == 119) /*&& data->dead == FALSE*/
-		//move_forward(&data);
+    if (kc == 100) /*&& data->dead == FALSE */
+		move_right(&data);
+	if (kc == 97)/*&& data->dead == FALSE*/
+		move_left(&data);
+	if (kc == 115) /*&& data->dead == FALSE*/	
+		move_backwards(&data);
+	if (kc == 119) /*&& data->dead == FALSE*/
+		move_forward(&data);
 }
--- cub3d/mlxste.c
+++ cub3d/mlxste.c
@@ -18,8 +18,6 @@ void	mlx_handler(t_data *data)
 			data->res_h, "CUB3D");
 	mlx_key_hook(data->window, keys_control, data);
 	mlx_hook(data->window, 17, 0, close_game, (void *)data);
-	//raycasting(data);
-	//mlx_put_image_to_window(data->mlx, data->window, data->image.img, 0, 0);
 	mlx_loop_hook(data->mlx, nullifiereee, (void *)data);
 	mlx_loop(data->mlx);
 }
@@ -38,28 +36,4 @@ int	nullifiereee(void *data)
 	raycasting(tmp);
 	mlx_put_image_to_window(tmp->mlx, tmp->window, tmp->image.img, 0, 0);
 	return (0);
-}
-
-void	print_map(t_data *data)
-{
-	char *img;
-
-//questa roba va fatta quando andiamo a controllare tutte le texure e le salviamo
-	//data->text.ceiling = mlx_new_image(data->mlx,
-	//		(data->res_w / 2), (data->res_h / 2));
-	//data->text.floor = mlx_new_image(data->mlx,
-	//		(data->res_w / 2), (data->res_h / 2));
-	//img = mlx_get_data_addr(data->text.ceiling, &data->text.bits_per_pixel,
-	//		&data->text.size_line, &data->text.endian);
-	//*(unsigned int *)img = data->ceiling_color;
-	//img = mlx_get_data_addr(data->text.floor, &data->text.bits_per_pixel,
-	//		&data->text.size_line, &data->text.endian);
-	//*(unsigned int *)img = data->floor_color;
-	//mlx_put_image_to_window(data->mlx, data->window, data->text.ceiling, 0, 0);
-	//mlx_put_image_to_window(data->mlx, data->window, data->text.floor, 639, 359);
-	// tutta questa roba serve a modificare il colore di un solo pixel in particolare,
-	//bisogna quindi anche per il ceiling e per il floor calcolare pixel per pixel,
-	//la soluzione più semplice è riempire di pixel il tutto, e pio andare a modificare
-	// quelli dei muri.
-	return ;
 }
