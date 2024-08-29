b6d133e2b4498d18e4b587d914ad4b0dcd883059
fratesco <fratesco0@gmail.com>
2024-08-29 11:18:40 +0200

    finito ex01

---
+++ cpp/cpp_module_07/ex01/iter.hpp
@@ -0,0 +1,11 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template <typename T, typename U, typename K>
+void iter(T *addr, U const len, K (*func)(T const &))
+{
+	for(U i = 0; i < len; i++)
+		func(addr[i]);
+}
+
+#endif
---
+++ cpp/cpp_module_07/ex01/main.cpp
@@ -0,0 +1,24 @@
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+
+void twice(int const &n) 
+{
+    std::cout << n * 2 << std::endl;
+}
+
+void print(std::string const &s) 
+{
+    std::cout << s << std::endl;
+}
+
+int main(void) 
+{
+    int array[] = {1, 2, 3, 4, 5};
+
+    std::string strings[] = {"s1", "s2", "s3"};
+
+    ::iter(array, 5, twice);
+
+    ::iter(strings, 3, print);
+}
