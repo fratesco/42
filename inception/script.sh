de29e1070cba8d0e5dc92b39af83df7d8286b2e3
fgolino <fgolino@student.42roma.it>
2025-05-15 15:18:17 +0200

    consegna inception

--- inception/script.sh
+++ inception/script.sh
@@ -26,11 +26,11 @@ echo "DOMAIN_NAME=fgolino.42.fr
 WP_ADMIN_N=boss
 WP_ADMIN_P="$str >> ./src/.env
 echo "WP_TITLE=Inception
-WP_ADMIN_E=fgolino.42.fr" >> ./src/.env
+WP_ADMIN_E=fgolino@42student.fr" >> ./src/.env
 echo >> ./src/.env
 
 random_string
 echo -n "WP_U_NAME=Stefano_Tamiano
 WP_U_PASS=$str 
 WP_U_ROLE=fastidio
-WP_U_EMAIL=Stamiano.42.fr" >> ./src/.env
+WP_U_EMAIL=Stamiano@42student.fr" >> ./src/.env
