a6189ed60bd1ae59e61d43416802607428d06276
fratesco <fratesco0@gmail.com>
2026-04-12 16:56:57 +0200

    ft_strcpy funzionante

---
+++ libasm/ft_strcpy.s
@@ -0,0 +1,22 @@
+default rel
+
+BITS 64
+
+global ft_strcpy
+
+SECTION .text
+
+ft_strcpy:
+            mov rax, -1
+            ALIGN 8
+            jmp _loop
+
+
+_loop:
+            add rax, 1
+            mov cl, byte[rsi+rax]
+            mov [rdi+rax], cl
+            cmp byte[rsi+rax], 0
+            jnz _loop
+            mov rax, rdi
+            ret
--- libasm/main.c
+++ libasm/main.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 
 size_t ft_strlen(char *str);
+char *ft_strcpy(char *dest, const char *src);
 
 int main(int argc, char **argv)
 {
-        printf("%zu\n", ft_strlen("CIAO"));
-        char *str = malloc(sizeof(char));
-        str[0] = ' ';
-        str[1] = 'c';
-        printf("%d\n", ft_strlen(str));
+        char *str = ft_strcpy(malloc(sizeof(char)*5), "CIAO");
+        printf("%zu %s\n", ft_strlen(str), str);
+        free(str);
         return(0);
 }       
